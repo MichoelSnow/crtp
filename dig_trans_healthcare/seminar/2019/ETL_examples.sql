0ba4ba8cccc29ddd903b89232fb451451935889f
Michoel Snow <protect3f@gmail.com>
2019-05-21 09:48:23 -0400

    added 2019 version of seminar

---
+++ dig_trans_healthcare/seminar/2019/ETL_examples.sql
@@ -0,0 +1,21 @@
+
+
+
+
+-- edm.lookupfinding
+-- FINDINGDESC
+
+-- edm.findings
+-- nativefinding, numericfinding, findingtypeid, numericfindinglow, numericfindinghigh
+
+
+
+
+
+
+-- Export and plot the results
+
+
+
+
+
